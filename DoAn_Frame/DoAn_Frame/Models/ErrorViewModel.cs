a0d9f4786b7c373a67253086d1cb110697998391
TranNhatTan0312 <19522177@gm.uit.edu.vn>
2023-01-06 21:44:16 +0700

    Up do an

---
+++ DoAn_Frame/DoAn_Frame/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DoAn_Frame.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
